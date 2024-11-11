29ae97d0f4f9ff1f88e34cc5a13e0ce8702d6be7
edith.teodocia@gmail.com <edith.teodocia@gmail.com>
2024-11-11 19:14:19 -0400

    #03:: Actualizando

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,6 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-abstract class Controller
-{
-    //
-=======
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -14,5 +9,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
->>>>>>> 8e067eff3492b4990e7506d24cf9716d21790751
 }
--- app/Models/Vaca.php
+++ app/Models/Vaca.php
@@ -1,29 +1,12 @@
 <?php
 
 namespace App\Models;
-<<<<<<< HEAD
-=======
-
->>>>>>> 8e067eff3492b4990e7506d24cf9716d21790751
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vaca extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-
-    protected $primaryKey = 'vaca_id';
-
-    public function productor()
-    {
-        return $this->belongsTo(Productor::class, 'productor_id');
-    }
-    public function produccionLeche()
-    {
-        return $this->hasMany(ProduccionLeche::class, 'vaca_id');
-    }
-=======
     protected $fillable = [
         'productor_id',
         'nombre',
@@ -33,5 +16,4 @@ class Vaca extends Model
         'fecha_nacimiento',
         'estado',
     ];
->>>>>>> 8e067eff3492b4990e7506d24cf9716d21790751
 }
